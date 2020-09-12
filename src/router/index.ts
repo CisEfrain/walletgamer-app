262953e9e4e6d81750048cd82d0e7a371218732e
michel novellino <michelnovellino.programador@gmail.com>
2020-09-12 18:33:56 -0400

    save login, register, routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -17,7 +17,19 @@ const routes: Array<RouteConfig> = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+      import( "../views/About.vue")
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: () =>
+      import( "../views/auth/Login.vue")
+  },
+  {
+    path: "/register",
+    name: "Register",
+    component: () =>
+      import( "../views/auth/Register.vue")
   }
 ];
 
