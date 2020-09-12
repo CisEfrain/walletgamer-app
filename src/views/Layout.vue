be2d1d50313f97a00015cadc6fee4b970dcc9f7e
michel novellino <michelnovellino.programador@gmail.com>
2020-09-12 19:47:52 -0400

    inputs test

---
+++ src/views/Layout.vue
@@ -0,0 +1,24 @@
+<template>
+  <router-view></router-view>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+@Component({
+  components: {}
+})
+export default class Home extends Vue {}
+</script>
+
+<style>
+.custom-divider {
+  height: 2px;
+  max-width: 40%;
+  margin: auto;
+}
+@media (max-width: 768px) {
+  .custom-divider {
+    max-width: 80%;
+  }
+}
+</style>
--- src/views/auth/Login.vue
+++ src/views/auth/Login.vue
@@ -1,5 +1,25 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <v-container class="mt-16 pt-16"  >
+      <v-row  align="center"   justify="center">
+          <h1 class="">
+              Ingreso a tu cuenta gamer
+          </h1> 
+      </v-row>
+
+          <hr class="custom-divider">
+    <v-row align="center"   justify="center">
+        
+      <v-col cols="6" md="4" >
+        <v-text-field placeholder="email"  solo color="success"  ></v-text-field>
+        <v-text-field placeholder="email"  solo color="success"  ></v-text-field>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
+<style scoped>
+   .v-input{
+        border-radius: 20px;
+    }
+ 
+</style>
+ 
