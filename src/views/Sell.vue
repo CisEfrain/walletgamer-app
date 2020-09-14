3794fc3d51c87c680b01f8c402245a6b5a2ab3c3
michel novellino <michelnovellino.programador@gmail.com>
2020-09-14 17:43:09 -0400

    save

--- src/views/Sell.vue
+++ src/views/Sell.vue
@@ -1,15 +1,61 @@
 <template>
-  <h2>Sell page view</h2>
+  <v-container>
+    <h2 class="custom-class">Sell page view</h2>
+    <v-row>
+      <v-col cols="12">
+        <v-stepper v-model="e1">
+          <v-stepper-header>
+            <v-stepper-step :complete="e1 > 1" step="1"></v-stepper-step>
+
+            <v-divider></v-divider>
+
+            <v-stepper-step :complete="e1 > 2" step="2"></v-stepper-step>
+
+            <v-divider></v-divider>
+
+            <v-stepper-step step="3"></v-stepper-step>
+          </v-stepper-header>
+
+          <v-stepper-items>
+            <v-stepper-content step="1">
+              <v-card class="mb-12" color="grey lighten-1" height="200px"></v-card>
+
+              <v-btn color="primary" @click="e1 = 2">Continue</v-btn>
+
+              <v-btn text>Cancel</v-btn>
+            </v-stepper-content>
+
+            <v-stepper-content step="2">
+              <v-card class="mb-12" color="grey lighten-1" height="200px"></v-card>
+
+              <v-btn color="primary" @click="e1 = 3">Continue</v-btn>
+
+              <v-btn text>Cancel</v-btn>
+            </v-stepper-content>
+
+            <v-stepper-content step="3">
+              <v-card class="mb-12" color="grey lighten-1" height="200px"></v-card>
+
+              <v-btn color="primary" @click="e1 = 1">Continue</v-btn>
+
+              <v-btn text>Cancel</v-btn>
+            </v-stepper-content>
+          </v-stepper-items>
+        </v-stepper>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from "vue-property-decorator";
-
+import { Component, Provide, Vue } from "vue-property-decorator";
 
 @Component
-export default class Sell extends Vue {}
+export default class Sell extends Vue {
+  e1 = 1;
+}
 </script>
 
-<style>
+<style lang="sass">
 
 </style>
