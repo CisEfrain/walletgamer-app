3bddb305f8d14669b8621b38068725d95f6d4061
michel novellino <michelnovellino.programador@gmail.com>
2020-09-16 09:12:19 -0400

    ts documentation example

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,3 +1,10 @@
+/**
+*@remarks 
+* this function can be changed 
+*@param identifier - string user or email
+*@param password - user password
+*@returns an object with jwt token and user info or error messages
+**/
 export const Login = function (identifier: string, password: string) {
     if(identifier === 'test' && password === '12345678') return false;
     
