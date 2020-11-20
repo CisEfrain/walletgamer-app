ed017bb4b23f542928b31d0a762a3b8b5c530b7a
michel novellino <michelnovellino.programador@gmail.com>
2020-11-19 20:02:49 -0400

    remove unused comments

--- src/router/index.ts
+++ src/router/index.ts
@@ -13,74 +13,47 @@ const routes: Array<RouteConfig> = [
       {
         path: "/about",
         name: "About",
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
         component: () => import("../views/About.vue")
       },
       {
         path: "/account",
         name: "Account",
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
         component: () => import("../views/Account.vue")
       },
       {
         path: "/buy",
         name: "Buy",
-        // route level code-splitting
-        // this generates a separate chunk (buy.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
         component: () => import("../views/Buy.vue")
       },
       {
         path: "/payment",
         name: "Payment",
-        // route level code-splitting
-        // this generates a separate chunk (buy.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
+
         component: () => import("../views/Payment.vue")
       },
       {
         path: "/sellment",
         name: "Sellment",
-        // route level code-splitting
-        // this generates a separate chunk (buy.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
         component: () => import("../views/Sellment.vue")
       },
       {
         path: "/help",
         name: "Help",
-        // route level code-splitting
-        // this generates a separate chunk (help.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
         component: () => import("../views/Help.vue")
       },
       {
         path: "/sell",
         name: "Sell",
-        // route level code-splitting
-        // this generates a separate chunk (sell.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
         component: () => import("../views/Sell.vue")
       },
       {
         path: "/",
         name: "Transactions",
         component: Transactions
-        // route level code-splitting
-        // this generates a separate chunk (transactions.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
-        // component: () => import("../views/Transactions.vue")
       },
       {
         path: "/wallet",
         name: "Wallet",
-        // route level code-splitting
-        // this generates a separate chunk (wallet.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
         component: () => import("../views/Wallet.vue")
       }
     ]
